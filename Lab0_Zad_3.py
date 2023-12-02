d9539c7af7cc36d306ea26655897d20e17bf2cf7
Xento123 <152697611+Xento123@users.noreply.github.com>
2023-12-02 13:46:46 +0100

    Delete Lab0_Zad_3.py

--- Lab0_Zad_3.py
+++
@@ -1,9 +0,0 @@
-bok1=int(input("Podaj długość pierwszego boku: "))
-
-bok2=int(input("Podaj długość drugiego boku "))
-
-pole=bok1*bok2
-obw=(bok1+bok2)*2
-print(type(obw))
-print("Pole prostokąta wynosi: ",pole)
-print("Obwód prostokąta wynosi: "+str(obw))
