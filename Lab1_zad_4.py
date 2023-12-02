0e8892cad69124d33f44120c164dfae77d55f92c
Xento123 <152697611+Xento123@users.noreply.github.com>
2023-12-02 15:04:00 +0100

    Add files via upload

---
+++ Lab1_zad_4.py
@@ -0,0 +1,18 @@
+import math
+print("Równanie w postaci a*x*2 + bx + c == 0")
+a = float(input("podaj a:"))
+b = float(input("Podaj b: "))
+c = float(input("Podaj c: "))
+delta = b**2 - 4 * a * c
+if delta > 0:
+    print("Pierwiastki równania kwadratowego: ")
+    x1 = (-b - math.sqrt(delta)) / (2 * a)
+    x2 = (-b + math.sqrt(delta)) / (2 * a)
+    print("x1: ", x1)
+    print("x2: ", x2)
+elif delta == 0:
+    print("Pierwiastek podwójny równania: ")
+    x0 = -b / (2 * a)
+    print("x0: ", x0)
+else:
+    print("Delta jest ujemna, wiec nie liczymy miejsc zerowych.")
