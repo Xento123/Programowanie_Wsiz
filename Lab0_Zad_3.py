b815cd69c2564b0547e11487f6cb335610117edf
Xento123 <152697611+Xento123@users.noreply.github.com>
2023-12-02 13:47:16 +0100

    Add files via upload

---
+++ Lab0_Zad_3.py
@@ -0,0 +1,11 @@
+bok1=int(input("Podaj długość pierwszego boku: "))
+
+bok2=int(input("Podaj długość drugiego boku "))
+
+pole=bok1*bok2
+obw=(bok1+bok2)*2
+print(type(obw))
+#print("Pole prostokąta wynosi: ",pole)
+#print("Obwód prostokąta wynosi: "+str(obw))
+print(f"Pole prostokąta wynosi {pole}")
+print(f"Obwód prostokąta wynosi {obw}")
